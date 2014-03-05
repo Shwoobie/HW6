02d0f92a4b0be9661d5daba25a186ec425bacadb
Shwoobie <atdwashington@gmail.com>
2014-03-05 01:56:43 -0800

    asdasd

--- Given/sched.pl
+++ Given/sched.pl
@@ -8,4 +8,42 @@ c_pl(N) :-
 
 %% numbers ofall nonprogramming languages courses
 c_notpl(N) :-
-		course(N,\+ programming_languages,_).
+
+		 course(N,_,_), \+(course(N,programming_languages,_)).
+
+
+
+
+
+%% list of those teaching
+c_inst60(L) :-
+
+		 course(60,_,L) mergesort(L,X).
+
+
+mergesort(Xs, S) :-
+    length(Xs, Len),
+    (Len =< 2 ->
+        S = Xs;
+        split_in_half(Xs, Ys, Zs),
+        mergesort(Ys, SY),
+        mergesort(Zs, SZ),
+        merge(SY, SZ, S)
+    ).
+
+% First two cases: merging any list Xs with an empty list yields Xs
+merge([], Xs, Xs).
+merge(Xs, [], Xs).
+merge(Xs, Ys, S) :- %compare lists
+    Xs = [X|Xs0],
+    Ys = [Y|Ys0],
+    (X @=< Y ->
+        S = [X|S0],
+        merge(Xs0, Ys, S0);
+        S = [Y|S0],
+        merge(Xs, Ys0, S0)).
+
+split_in_half(Xs, Ys, Zs) :-
+    length(Xs, Len),
+    Half is Len // 2,    % integer division, rounding down
+    split_at(Xs, Half, Ys, Zs).
